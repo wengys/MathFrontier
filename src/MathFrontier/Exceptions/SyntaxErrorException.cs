18149c9bd529650e01588deadbad37092f8e5d12
wengys <wengys@teleware.cn>
2017-10-10 14:19:27 +0800

    增加生成文档

--- src/MathFrontier/Exceptions/SyntaxErrorException.cs
+++ src/MathFrontier/Exceptions/SyntaxErrorException.cs
@@ -2,10 +2,18 @@
 
 namespace MathFrontier.Exceptions
 {
+    /// <summary>
+    /// 表达式格式错误
+    /// </summary>
     public class SyntaxErrorException : Exception
     {
+        /// <summary>
+        /// 创建实例
+        /// </summary>
+        /// <param name="phase">阶段</param>
+        /// <param name="message">信息</param>
         public SyntaxErrorException(string phase, string message)
-            : base($"SyntaxError {phase} {message}")
+            : base($"{phase} {message}")
         {
         }
     }
